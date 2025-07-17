04f423369dcd0e0ea311fa32518f24f7183099b5
YuvrajShaktawat <yuvrajshaktawat421@gmail.com>
2025-07-17 11:31:11 +0530

    Initial commit

---
+++ src/main/java/com/practise/spring/learn_JPA_Spring/LearnJpaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.practise.spring.learn_JPA_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practise/spring/learn_JPA_Spring/LearnJpaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.spring.learn_JPA_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
